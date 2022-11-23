a93317382259e9ec4aec3dc22187ae5b40c96303
collyrium921 <kajusinghal921@gmail.com>
2022-11-23 15:22:34 +0530

    -view encapsulation for single and multiple child components in markup

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { BlogPost } from '../blog-post';
+import { BlogPostTileComponent } from '../blog-post-tile/blog-post-tile.component';
 import { BLOG_POSTS } from '../blogPost';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
-  styleUrls: ['./blog-list.component.scss']
+  styleUrls: ['./blog-list.component.scss'],
 })
 export class BlogListComponent implements OnInit {
   blogPosts!: BlogPost[][];
-  currentPage:number=0;
-  constructor() { }
+  currentPage: number = 0;
+  //for one component
+  // @ViewChild('tile') blogPostTileComponent!: BlogPostTileComponent;
+
+  //for multiple components used in markup
+  @ViewChildren('tile')
+  blogPostTileComponent!: QueryList<BlogPostTileComponent>;
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.blogPosts = BLOG_POSTS
+    this.blogPosts = BLOG_POSTS;
   }
 
-  updatePage(newPage:number){
+  updatePage(newPage: number) {
     this.currentPage = newPage;
-    }
+  }
+
+  expandAll() {
+    //for a single component used in markup
+    // this.blogPostTileComponent.showFullSummary()
+
+    //for multiple components used in markup
+    this.blogPostTileComponent.forEach((e) => e.showFullSummary());
+  }
 }
